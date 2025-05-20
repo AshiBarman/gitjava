bb47fcc7067b1e48020cb1b55538a913e5090897
AshiBarman <ashibarman567@gmail.com>
2025-05-20 00:10:10 -0700

    Question10

--- Question10.java
+++ Question10.java
@@ -1,4 +1,16 @@
-public class Question10 {
+import java.util.Arrays;
+public class Question10{
+    public static int findDuplicate(int [] nums) {
+        Arrays.sort(nums);
+        for(int i = 1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        int [] arr ={3,4,5,5,7};   
+        int [] nums = {4,2,5,3,2,1};
+        System.out.println("Duplicate"+"-"+findDuplicate(nums));
+    }
 }
